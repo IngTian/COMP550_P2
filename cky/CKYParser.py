ffea829ca56430bba02a03255f97b8655de18bd5
IngTian <tzy20001018@gmail.com>
2021-11-14 18:41:49 -0500

    feat: completed cnf parsing

--- cky/CKYParser.py
+++ cky/CKYParser.py
@@ -2,259 +2,185 @@ import random
 
 from nltk import CFG
 from nltk.grammar import Production, Nonterminal
-from typing import List, Dict, Union, Tuple
+from nltk.tree import Tree
+from typing import List, Dict, Union, Tuple, Set
+from enum import Enum
 
 
-class CGFStatement:
-    rhs: Tuple[Union[Nonterminal, str]]
-    lhs: Nonterminal
-    is_terminal: bool
-
-
-class CNFStatement:
-    rhs: Tuple[Union[Nonterminal, str]]
-    lhs: Nonterminal
-    is_terminal: bool
-    map: "CGFToCNFMap"
-
-
-class CGFToCNFMap:
-    cgf: List[List[CGFStatement]]
-    cnf: List[CNFStatement]
-
-
-class CKYDPBlockEntry:
-    cnf_statement: CNFStatement
-    is_terminal: bool
-    descendents: List["CKYDPBlockEntry"]
-
-
-class CNFContainer:
-
-    def __init__(self, cnf_map: Dict[Nonterminal, Dict[str, List[CGFToCNFMap]]]):
-        pass
-
-
-def clone_map(m: CGFToCNFMap) -> CGFToCNFMap:
-    chain_of_cnf: List[CNFStatement] = list()
-    list_of_chain_of_cgf: List[List[CGFStatement]] = list()
-
-    for cnf in m.cnf:
-        new_cnf = CNFStatement()
-        new_cnf.rhs = cnf.rhs
-        new_cnf.lhs = cnf.lhs
-        new_cnf.is_terminal = cnf.is_terminal
-        chain_of_cnf.append(new_cnf)
-
-    for chain in m.cgf:
-        new_chain = chain.copy()
-        list_of_chain_of_cgf.append(new_chain)
-
-    new_map = CGFToCNFMap()
-    new_map.cgf = list_of_chain_of_cgf
-    new_map.cnf = chain_of_cnf
-
-    return new_map
+class RuleType(Enum):
+    TERMINAL = 1
+    UNARY = 2
+    BINARY = 3
+    BINARY_PLUS = 4
 
 
 class CKYParser:
 
     def __init__(self, grammar: CFG):
-        self.__original_cgf = grammar
-        self.__formatted_cgf = self.__format_grammar()
-        self.__cnf_container = self.__parse_cnf()
+        self.__original_grammar = grammar
+        self.__organized_cfg_grammar = CKYParser.__organize_grammar(grammar.productions())
 
-    def __format_grammar(self) -> Dict[Nonterminal, List[CGFStatement]]:
+        # This table records all possible unary paths, given the start point and the end point.
+        # The key follows the format: <Non-terminal> -> <EndPoint1>,<EndPoint2>,<EndPoint3>.
+        # e,g. A -> B,C / A -> 'ne',B,C
+        self.__unary_chain_table: Dict[str, List[List[Production]]] = dict()
+        self.__cnf_grammar = None
+        self.__initialize_cnf()
+
+    def parse(self, sentence: str) -> List[Tree]:
+        pass
+
+    def __initialize_cnf(self) -> None:
+        cnf_rules: List[Production] = list()
+        for cgf_rule in self.__original_grammar.productions():
+            cnf_rules += CKYParser.__build_binary_plus_cnf(cgf_rule)
+        organized_cnf = CKYParser.__organize_grammar(cnf_rules)
+        node_reachable_table: Dict[Nonterminal, List[List[Production]]] = dict()
+        for starting_point in organized_cnf.keys():
+            CKYParser.__parse_unary_rules(starting_point, node_reachable_table, organized_cnf)
+
+        def stringify_list_of_nodes(node: Union[Nonterminal, str]) -> str:
+            return f"'{node}'" if type(node) == str else str(node)
+
+        for starting_point in node_reachable_table.keys():
+            for paths in node_reachable_table.get(starting_point):
+                new_unary_rules: Set[Production] = set()
+                for path in paths:
+                    # Non-unary
+                    if len(path) <= 1:
+                        continue
+                    # Unary
+                    path_identifier = f"{str(path[0].lhs())} -> {','.join(map(stringify_list_of_nodes, path[-1].rhs()))}"
+                    if path_identifier in self.__unary_chain_table.keys():
+                        self.__unary_chain_table.get(path_identifier).append(path)
+                    else:
+                        self.__unary_chain_table[path_identifier] = [path]
+                    new_unary_rules = new_unary_rules.union([Production(path[0].lhs(), path[-1].rhs())])
+
+        self.__cnf_grammar = CFG(self.__original_grammar.start(), cnf_rules)
+
+    @staticmethod
+    def __build_binary_plus_cnf(rule: Production) -> List[Production]:
         """
-        Convert the raw CGF grammar into a map.
-        :return: A map of rules.
+        Convert a Binary+ rule into CNF.
+        :param rule: A binary plus rule.
+        :return: A list of CNF.
         """
-        rules: List[Production] = self.__original_cgf.productions()
-        result: Dict[Nonterminal, List[CGFStatement]] = dict()
+        lhs, rhs = rule.lhs(), rule.rhs()
+        results: List[Production]
 
-        for rule in rules:
-            new_cgf = CGFStatement()
-            new_cgf.lhs, new_cgf.rhs = rule.lhs(), rule.rhs()
-            new_cgf.is_terminal = True if len(new_cgf.rhs) == 1 and type(new_cgf.rhs[0]) == str else False
-
-            # Add the new CGF to the map.
-            if new_cgf.lhs in result.keys():
-                result.get(new_cgf.lhs).append(new_cgf)
+        # If terminal or binary, simply return.
+        if CKYParser.__is_binary(rule) or CKYParser.__is_terminal(rule) or CKYParser.__is_unary(rule):
+            results = [Production(lhs, rhs)]
+        # If the rhs is binary_plus, do the following.
+        else:
+            rhs_length = len(rhs)
+            rhs_first_half = rhs[:rhs_length - 1]
+            rhs_first_half_symbol = Nonterminal(CKYParser.__get_cnf_non_terminal_id(rhs_first_half))
+            first_half_temp_cnf = Production(rhs_first_half_symbol, rhs_first_half)
+            first_half_rules = CKYParser.__build_binary_plus_cnf(first_half_temp_cnf)
+            if type(rhs[-1]) == Nonterminal:
+                results = [Production(lhs, [rhs_first_half_symbol, rhs[-1]])] + first_half_rules
             else:
-                result[new_cgf.lhs] = [new_cgf]
+                second_half_rule = Production(Nonterminal(CKYParser.__get_cnf_non_terminal_id([rhs[-1]])), rhs[-1])
+                return [Production(lhs, [rhs_first_half_symbol, second_half_rule.lhs()])] + first_half_rules + [
+                    second_half_rule]
+
+        return results
+
+    @staticmethod
+    def __get_cnf_non_terminal_id(rhs: List[Union[Nonterminal, str]]) -> str:
+        return f'CNF-Symbol-Replace-{"-".join(map(lambda s: str(s), rhs))}-{random.randint(0, 10000)}'
+
+    @staticmethod
+    def __parse_unary_rules(
+            node: Nonterminal,
+            node_reachable_table: Dict[Nonterminal, List[List[Production]]],
+            organized_grammar: Dict[Nonterminal, Dict[RuleType, List[Production]]]
+    ) -> List[List[Production]]:
+        """
+        Parse unary rules.
+        :param node: The starting non-terminal node.
+        :param node_reachable_table: A table.
+        :param organized_grammar: Organized grammar.
+        :return: List of possible paths from the starting node.
+        """
+
+        if node in node_reachable_table.keys():
+            return node_reachable_table.get(node)
+
+        # Concatenate all terminal, binary, and binary-plus productions.
+        result: List[List[Production]] = list()
+        node_cfg_record = organized_grammar.get(node)
+        for terminal_rule in node_cfg_record.get(RuleType.TERMINAL):
+            result.append([terminal_rule])
+
+        for binary in node_cfg_record.get(RuleType.BINARY):
+            result.append([binary])
+
+        for binary_plus_rule in node_cfg_record.get(RuleType.BINARY_PLUS):
+            result.append([binary_plus_rule])
+
+        unary_paths: List[List[Production]] = list()
+        for unary_rule in node_cfg_record.get(RuleType.UNARY):
+            target_node = unary_rule.rhs()[0]
+            paths = CKYParser.__parse_unary_rules(target_node, node_reachable_table, organized_grammar)
+            for path in paths:
+                cloned_path = path.copy()
+                cloned_path.insert(0, unary_rule)
+                unary_paths.append(cloned_path)
+
+        result += unary_paths
+
+        node_reachable_table[node] = result
 
         return result
 
-    def __parse_cnf(self) -> CNFContainer:
+    @staticmethod
+    def __organize_grammar(rules: List[Production]) -> Dict[Nonterminal, Dict[RuleType, List[Production]]]:
         """
-        Convert the formatted CGF into
-        CNFs.
-        :return: A CNF Container which contains all CNF statements.
+        Organize each CFG rule into 4 categories.
+        Terminal:    A -> 'word'
+        UNARY:       A -> B
+        BINARY:      A -> B C
+        BINARY_PLUS: A -> B C D / A -> B 'ne' D / A -> 'ne' B
+        :return: Organized CGF rules.
         """
-        result: List[CNFStatement] = list()
-        cgf_rules_lookup_table: Dict[Nonterminal, Dict[str, List[CGFStatement]]] = dict()
-        cgf_cnf_map_lookup_table: Dict[Nonterminal, Dict[str, List[CGFToCNFMap]]] = dict()
+        result: Dict[Nonterminal, Dict[RuleType, List[Production]]] = dict()
+        for rule in rules:
+            assert isinstance(rule, Production)
+            lhs, rhs = rule.lhs(), rule.rhs()
+            if lhs not in result.keys():
+                result[lhs] = {
+                    RuleType.TERMINAL: [],
+                    RuleType.UNARY: [],
+                    RuleType.BINARY: [],
+                    RuleType.BINARY_PLUS: [],
+                }
 
-        def extract_rules(rules: List[CGFStatement]) -> Dict[str, List[CGFStatement]]:
-            """
-            Extract various CGF rules into 4 categories.
-            1. Terminal
-            2. Unary
-            3. Binary: Both of RHS nodes has to be Non-terminal.
-            4. Binary+: Everything else.
-            :param rules: CGF rules.
-            :return: Organized rules.
-            """
-            temp: Dict[str, List[CGFStatement]] = {
-                "terminal": [],
-                "unary": [],
-                "binary": [],
-                "binary+": [],
-            }
+            if CKYParser.__is_terminal(rule):
+                result[lhs][RuleType.TERMINAL].append(rule)
+            elif CKYParser.__is_unary(rule):
+                result[lhs][RuleType.UNARY].append(rule)
+            elif CKYParser.__is_binary(rule):
+                result[lhs][RuleType.BINARY].append(rule)
+            elif CKYParser.__is_binary_plus(rule):
+                result[lhs][RuleType.BINARY_PLUS].append(rule)
 
-            for rule in rules:
-                if rule.is_terminal:
-                    temp["terminal"].append(rule)
-                elif len(rule.rhs) == 1 and type(rule.rhs[0]) == Nonterminal:
-                    temp["unary"].append(rule)
-                elif len(rule.rhs) == 2 and type(rule.rhs[0]) == Nonterminal and type(rule.rhs[0]) == Nonterminal:
-                    temp["binary"].append(rule)
-                else:
-                    temp["binary+"].append(rule)
+        return result
 
-            return temp
+    @staticmethod
+    def __is_terminal(rule: Production) -> bool:
+        return rule.is_lexical() and len(rule.rhs()) == 1
 
-        def build_cgf_cnf_map(starting_point: Nonterminal) -> Dict[str, List[CGFToCNFMap]]:
+    @staticmethod
+    def __is_unary(rule: Production) -> bool:
+        return rule.is_nonlexical() and len(rule.rhs()) == 1
 
-            if starting_point in cgf_cnf_map_lookup_table.keys():
-                return cgf_cnf_map_lookup_table.get(starting_point)
+    @staticmethod
+    def __is_binary(rule: Production) -> bool:
+        return rule.is_nonlexical() and len(rule.rhs()) == 2
 
-            cgf_rules = cgf_rules_lookup_table.get(starting_point)
-            terminal = cgf_rules["terminal"]
-            unary = cgf_rules["unary"]
-            binary = cgf_rules["binary"]
-            binary_plus = cgf_rules["binary_plus"]
-
-            terminal_cnf_maps: List[CGFToCNFMap] = list()
-            unary_cnf_maps: List[CGFToCNFMap] = list()
-            binary_cnf_maps: List[CGFToCNFMap] = list()
-            binary_plus_cnf_maps: List[CGFToCNFMap] = list()
-
-            # Build terminal, binary, and binary+ rules first.
-            for terminal_rule in terminal:
-                new_map, cnf = CGFToCNFMap(), CNFStatement()
-                cnf.lhs = terminal_rule.lhs, cnf.rhs = terminal_rule.rhs
-                cnf.map = new_map
-                new_map.cgf.append([terminal_rule])
-                new_map.cnf.append(cnf)
-                terminal_cnf_maps.append(new_map)
-
-            for binary_rule in binary:
-                new_map, cnf = CGFToCNFMap(), CNFStatement()
-                cnf.lhs = binary_rule.lhs, cnf.rhs = binary_rule.rhs
-                cnf.map = new_map
-                new_map.cgf.append([binary_rule])
-                new_map.cnf.append(cnf)
-                binary_cnf_maps.append(new_map)
-
-            for binary_plus_rule in binary_plus:
-                new_map = CGFToCNFMap()
-                new_map.cgf.append([binary_plus_rule])
-                cnf_rules = build_binary_plus_cnf(binary_plus_rule)
-                new_map.cnf = cnf_rules
-                for cnf in cnf_rules:
-                    cnf.map = new_map
-                binary_plus_cnf_maps.append(new_map)
-
-            # Handle unary rules.
-            for unary_rule in unary:
-                rhs = unary_rule.rhs
-                assert isinstance(rhs, Nonterminal)
-                rhs_cnf = build_cgf_cnf_map(rhs)
-
-                unary_cnf = rhs_cnf.get("unary")
-                terminal_cnf = rhs_cnf.get("terminal")
-                binary_cnf = rhs_cnf.get("binary")
-                binary_plus_cnf = rhs_cnf.get("binary+")
-
-                # Concatenate terminal
-                for terminal_map in terminal_cnf:
-                    cloned_map = clone_map(terminal_map)
-                    cloned_map.cgf[0][0].lhs = starting_point
-                    cloned_map.cnf[0].lhs = starting_point
-                    unary_cnf_maps.append(cloned_map)
-
-                # Concatenate binary
-                for binary_map in binary_cnf:
-                    cloned_map = clone_map(binary_map)
-                    cloned_map.cgf[0][0].lhs = starting_point
-                    cloned_map.cnf[0].lhs = starting_point
-                    unary_cnf_maps.append(cloned_map)
-
-                # Concatenate binary+
-                for binary_plus_map in binary_plus_cnf:
-                    cloned_map = clone_map(binary_plus_map)
-                    cloned_map.cgf[0][0].lhs = starting_point
-                    cloned_map.cnf[0].lhs = starting_point
-                    unary_cnf_maps.append(cloned_map)
-
-                # Concatenate unary
-                for unary_map in unary_cnf:
-                    cloned_map = clone_map(unary_map)
-                    for list_of_chain_cgf in cloned_map.cgf:
-                        list_of_chain_cgf.insert(0, unary_rule)
-                    cloned_map.cnf[0].lhs = starting_point
-                    unary_cnf_maps.append(cloned_map)
-
-            return {
-                "terminal": terminal_cnf_maps,
-                "unary": unary_cnf_maps,
-                "binary": binary_cnf_maps,
-                "binary+": binary_plus_cnf_maps
-            }
-
-        def build_binary_plus_cnf(statement: Union[CGFStatement, CNFStatement]) -> List[CNFStatement]:
-            lhs, rhs = statement.lhs, statement.rhs
-            result_cnf = CNFStatement()
-            result_cnf.lhs = lhs
-
-            # If the rhs consists of two terminal nodes, simply return.
-            if len(rhs) == 2 and type(rhs[0]) == Nonterminal and type(rhs[1]) == Nonterminal:
-                result_cnf.rhs = rhs
-                return [result_cnf]
-            # If the rhs is a terminal node, simply return.
-            elif len(rhs) == 1 and type(rhs[0]) == str:
-                result_cnf.rhs = rhs
-                return [result_cnf]
-            # If the rhs is binary_plus, do the following.
-            else:
-                rhs_length = len(rhs)
-                rhs_first_half = rhs[:rhs_length - 1]
-                rhs_first_half_str = "-".join(map(lambda s: str(s), rhs_first_half))
-                rhs_first_half_symbol = Nonterminal(
-                    f'CNF-Symbol-From-{str(lhs)}-To-{rhs_first_half_str}-{random.randint(0, 10000)}')
-                first_half_temp_cnf = CNFStatement()
-                first_half_temp_cnf.lhs = rhs_first_half_symbol
-                first_half_temp_cnf.rhs = rhs_first_half
-                first_half_rules = build_binary_plus_cnf(first_half_temp_cnf)
-                if type(rhs[-1]) == Nonterminal:
-                    result_cnf.rhs = [rhs_first_half_symbol, rhs[-1]]
-                    return [result_cnf] + first_half_rules
-                else:
-                    second_half_rule = CNFStatement()
-                    second_half_rule.rhs = rhs[-1]
-                    second_half_rule.lhs = Nonterminal(
-                        f'CNF-Symbol-From-{str(lhs)}-To-{rhs[-1]}-{random.randint(0, 10000)}')
-                    result_cnf.rhs = [rhs_first_half_symbol, second_half_rule.lhs]
-                    return [result_cnf] + first_half_rules + [second_half_rule]
-
-        # Sort all rules.
-        for cgf_lhs in self.__formatted_cgf.keys():
-            cgf_rules_lookup_table[cgf_lhs] = extract_rules(self.__formatted_cgf.get(cgf_lhs))
-
-        for node in self.__formatted_cgf.keys():
-            build_cgf_cnf_map(node)
-
-        new_container = CNFContainer(cgf_cnf_map_lookup_table)
-
-        return new_container
+    @staticmethod
+    def __is_binary_plus(rule: Production) -> bool:
+        return len(rule.rhs()) >= 2 and not CKYParser.__is_binary(rule)
