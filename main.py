73607d2d1c4c7bb5e676959a958614bf5ab6953e
IngTian <tzy20001018@gmail.com>
2021-11-16 13:05:51 -0500

    feat: added more proper nouns

--- main.py
+++ main.py
@@ -45,53 +45,6 @@ def testing_parse_sentences():
     parser.parse("e noodles with e e e e")
 
 
-def parse_sentences(parser: CKYParser, sentences: List[str]):
-    all = []
-    all_true = True
-    for s in sentences:
-        trees = parser.parse(s)
-        all.append(trees)
-        print(f"Sentence: '{s}'")
-        for t in trees:
-            t = Tree.fromstring(str(t))
-            t.pretty_print(unicodelines=True, nodedist=2)
-            print(str(t) + '\n')
-        if len(trees) == 0:
-            print("The sentence is not accepted.\n")
-            all_true = False
-    if all_true:
-        print("All accepted.")
-    return all
-
-
 if __name__ == '__main__':
-    with open('cfg.txt') as f:
-        lines = f.readlines()
-    grammar = CFG.fromstring(lines)
-    parser = CKYParser(grammar)
-    s_accept = [
-        'je regarde la television',
-        'tu regardes la television',
-        'il regarde la television',
-        'nous regardons la television',
-        'vous regardez la television',
-        'ils regardent la television',
-        'tu ne regardes pas la television',
-        'il la regarde',
-        'Jonathan aime le petit chat',
-        'Jonathan aime les chats noirs',
-        'je aime le Canada',
-        'le beau chat le mange',
-        'les aides aiment Montreal',
-    ]
-
-    s_reject = [
-        'je mangent le poisson',
-        'les noirs chats mangent le poisson',
-        'la poisson mangent les chats',
-        'je mange les',
-    ]
-    print("The following sentences should be accepted")
-    parse_sentences(parser, s_accept)
-    print("\nThe following sentences should be rejected")
-    parse_sentences(parser, s_reject)
+    testing_cnf_conversion()
+    testing_parse_sentences()
